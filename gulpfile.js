1c7858878abc4d8f1c3238d5e26c89725d0e8133
Slesarev Sergei <slesarev071290@gmail.com>
2016-04-21 12:30:56 +0300

    sprites

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+// Grab our gulp packages
+var gulp  = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    plumber = require('gulp-plumber'),
+    spritesmith = require('gulp.spritesmith')
+
+// Compile Sass, Autoprefix and minify
+gulp.task('styles', function() {
+  return gulp.src('./assets/scss/**/*.scss')
+    .pipe(plumber(function(error) {
+            gutil.log(gutil.colors.red(error.message));
+            this.emit('end');
+    }))
+    .pipe(sass())
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+    .pipe(gulp.dest('./assets/css/')) 
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+
+});
+
+// Watch files for changes
+gulp.task('watch', function() {
+  gulp.watch('./assets/scss/**/*.scss', ['styles']);
+});
+
+// Create a default task 
+gulp.task('default', function() {
+  gulp.start('styles');
+});
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('./assets/images/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.scss'
+  }));
+  return spriteData.pipe(gulp.dest('./assets/sprites/'));
+});
